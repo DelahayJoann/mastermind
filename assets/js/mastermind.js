5c5df8aa690e3cd151c2bac81b3d247019da0d98
Delahay Joann <delahay.joann@gmail.com>
2020-10-30 12:56:08 +0100

    add colorPicker

--- assets/js/mastermind.js
+++ assets/js/mastermind.js
@@ -1,13 +1,14 @@
 window.onload = function () {
-    // HTML pick
+    // HTML Selections
     let html_reponsesHistory = document.getElementById('reponsesHistory');
     let html_colorsPicker = document.getElementById('colorsPicker');
     let html_currentReponse = document.getElementById('currentReponse');
     let html_colorPick_array = document.querySelectorAll('.colorPick');
+
+    // Colors
     let colors = ["green","yellow","red","blue","orange","purple"];
 
-    console.log(html_colorPick_array);
-    // Add HTML colorPick
+    // Build HTML colorPick
     html_colorPick_array.forEach(function(colorPick,i){
         colorPick.style.backgroundColor = colors[i];
         colorPick.id = 'pick_'+colors[i];
